c9401b3df5086705536d57685bf7c9728005e3f7
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-30 19:43:19 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,42 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < coupons.length
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+  coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
+  cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
+  if cart_item && cart_item[:count] >= coupons[counter][:num]
+    if cart_item_with_coupon
+      cart_item_with_coupon[:count] += coupons[counter][:num]
+      cart_item[:count] -= coupons[counter][:num]
+    else
+      cart_item_with_coupon = {
+      :item => coupon_item_name,
+      :price => coupons[counter][:cost] / coupons[counter][:num],
+      :count => coupons[counter][:num],
+      :clearance => cart_item[:clearance]
+    }
+    cart << cart_item_with_coupon
+    cart_item[:count] -= coupons[counter][:num]
+  end
+end
+counter += 1
+end
+cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < cart.length
+    discount = cart[counter][:price] * .2
+    if cart[counter][:clearance] == "true"
+      cart[counter][:price] = cart[counter][:price] - discount
+      cart[counter][:price].round
+end
+counter += 1
+end
+cart
 end
 
 def checkout(cart, coupons)
